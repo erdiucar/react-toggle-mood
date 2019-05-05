ab57edc5b544d8d25104efda5287c2a1052469a7
Erdi Uçar <ucarerdi@gmail.com>
2019-05-05 19:01:59 +0300

    Updates

---
+++ precache-manifest.6fd2b2d1aa62d4c20c58794a89f1d6b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c883aceb8cfb254064ff37b4d0da4a9a",
+    "url": "/react-toggle-mood/index.html"
+  },
+  {
+    "revision": "8b34d7712ee952ad9079",
+    "url": "/react-toggle-mood/static/css/main.bdab3d78.chunk.css"
+  },
+  {
+    "revision": "61433e735fbb2d010e13",
+    "url": "/react-toggle-mood/static/js/2.b7f52e28.chunk.js"
+  },
+  {
+    "revision": "8b34d7712ee952ad9079",
+    "url": "/react-toggle-mood/static/js/main.b7b09756.chunk.js"
+  },
+  {
+    "revision": "77ea86ead756165e4b03",
+    "url": "/react-toggle-mood/static/js/runtime~main.1d2014a7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-toggle-mood/static/media/logo-bad.5d5d9eef.svg"
+  },
+  {
+    "revision": "d762553a1887dacf7ff9134a7229047c",
+    "url": "/react-toggle-mood/static/media/logo-good.d762553a.svg"
+  }
+]);
