9e098c2d6754a22258daa5684a6269fdb617bf99
Erdi Uçar <ucarerdi@gmail.com>
2019-05-05 18:59:11 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logoGood from "./logo-bad.svg";
+import logoBad from "./logo-good.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mood: "Good"
+    };
+  }
+
+  toggle() {
+    const moodState = this.state.mood === "Good" ? "Bad" : "Good";
+
+    this.setState({
+      mood: moodState
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header
+          className={
+            this.state.mood === "Good" ? "App-header good" : "App-header bad"
+          }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <img
+            src={this.state.mood === "Good" ? logoGood : logoBad}
+            className="App-logo"
+            alt="logo"
+          />
+
+          <button
+            className={this.state.mood === "Good" ? "btn bad" : "btn good"}
+            onClick={() => this.toggle()}
+          >
+            {this.state.mood}
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
